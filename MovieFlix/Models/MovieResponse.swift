f28be584bef618767c4dbdff8245f2eefd9b1400
Angelos Staboulis <angelosstampoulisgithub@gmail.com>
2026-08-24 22:56:57 +0300

    Commit rename a folder

---
+++ MovieFlix/Models/MovieResponse.swift
@@ -0,0 +1,12 @@
+//
+//  MovieResponse.swift
+//  MovieFlix
+//
+//  Created by Angelos Staboulis on 24/8/26.
+//
+
+import Foundation
+struct MovieResponse: Codable {
+    let results: [Movie]
+}
+
